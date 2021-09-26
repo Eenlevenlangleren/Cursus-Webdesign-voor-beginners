b86816d70478d7c7c2af3da730b3d96d55695c2b
office-assistant <83004209+Eenlevenlangleren@users.noreply.github.com>
2021-09-26 13:27:57 +0200

    Add files via upload
    JavaScript opdrachten

---
+++ Javascript/code.js
@@ -0,0 +1,30 @@
+// if mag gebruikt worden als variabele, code wordt duidelijker en bondiger
+console.log("Hello Winc Academy");
+console.log("Suzanne");
+function testNum(a) {
+  let result;
+  if (a > 0) {
+    result = "positive";
+  } else {
+    result = "NOT positive";
+  }
+  return result;
+}
+
+console.log(testNum(-5));
+// expected output: "NOT positive"
+
+console.log(4 + 4);
+// werkt niet voor strings
+
+console.log(5 + 5);
+console.log(10 - 9);
+console.log(3 * 3);
+console.log(6 / 2);
+console.log(10 % 100);
+
+let age = "55";
+console.log(55);
+
+let name = "vijfenvijftig";
+console.log(vijfenvijftig);
