3bc077d250cd13f15a4f890489db2e7acdc94656
office-assistant <83004209+Eenlevenlangleren@users.noreply.github.com>
2021-10-08 16:35:24 +0200

    Add files via upload
    rekensommen en comments loggen naar de terminal

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("Hallo Winc Academy Studenten");
+
+//"Dit is een grote som die je zo niet ziet in de terminal, comment on-line"
+/*let multiplication = 1230941 * 1823794;
+console.log ("multiplication:", multiplication);
+let division = 637263 / 54;
+console.log ("division:", division); ook dit zie je zo niet in de terminal, comment geblockt*/
