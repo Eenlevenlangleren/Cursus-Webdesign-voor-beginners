bc2300c9738aca0f9b216ce12a5c25a1bdd2271b
office-assistant <83004209+Eenlevenlangleren@users.noreply.github.com>
2021-10-09 18:04:09 +0200

    Add files via upload
    if/else statements

---
+++ if else statements.js
@@ -0,0 +1,24 @@
+
+const age = 18;
+    
+    if (age >= 18) 
+    {console.log('De persoon mag naar binnen');}
+
+    else 
+    {console.log('De persoon is te jong en mag niet naar binnen');}
+
+const isFemale  = Boolean;
+
+    if (Boolean=true)
+    {console.log("Welkom op LadiesNight");}
+
+    else 
+    {console.log("Heren zijn niet welkom");}
+
+const driverStatus = 'bob';
+
+    if  (driverStatus='bob')
+    {console.log("je mag rijden");}
+
+    else
+    {console.log("Je hebt teveel gedronken, je mag niet rijden");}
