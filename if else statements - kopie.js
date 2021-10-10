961a6622553cd20f88826df4f0d89a3dd952a4dc
office-assistant <83004209+Eenlevenlangleren@users.noreply.github.com>
2021-10-10 19:03:38 +0200

    Add files via upload
    Comparison and logical operators

---
+++ if else statements - kopie.js
@@ -0,0 +1,37 @@
+const age = 18;
+    
+if (age >= 18) 
+{console.log('De persoon mag naar binnen');}
+else 
+{console.log('De persoon is te jong en mag niet naar binnen');}
+
+const isFemale  = Boolean;
+
+if (Boolean=true)
+{console.log("Welkom op LadiesNight");}
+else 
+{console.log("Heren zijn niet welkom");}
+
+const driverStatus = 'bob';
+
+if  (driverStatus='bob')
+{console.log("je mag rijden");}
+else
+{console.log("Je hebt teveel gedronken, je mag niet rijden");}
+
+// comparison and logical operators
+
+if (age>=18 && age <=25)
+{console.log("Je krijgt 50% korting");}
+else {console.log("Je krijgt geen 50% korting");}
+
+
+if(firstName="Bram"||firstName="Sarah");
+ {console.log("Je hebt recht op een gratis biertje")};
+
+else 
+{console.log("Je hebt geen recht op een gratis biertje")};
+
+
+ 
+
