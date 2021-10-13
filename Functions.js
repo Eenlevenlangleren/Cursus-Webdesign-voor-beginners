2301ad73fa87e21539bcb6c845675709ce02b964
office-assistant <83004209+Eenlevenlangleren@users.noreply.github.com>
2021-10-13 19:04:18 +0200

    Add files via upload
    Functions

---
+++ Functions.js
@@ -0,0 +1,35 @@
+// Make a function - After making a function you have to use the function to see it in the console.
+const pizzaMediterrane = function () {
+console.log ("Buy ingredients");
+console.log ("Make dough");
+console.log ("Cut cheese");
+console.log ("Prepare and cut vegetables");
+console.log ("Top the pizza")
+}
+// Using a function
+pizzaMediterrane ();
+
+const number = 3;
+
+pizzaMediterrane ();
+pizzaMediterrane ();
+pizzaMediterrane ();
+
+// Making a function
+
+const sushilover = function () {
+    console.log ("Buy ingredients");
+    console.log ("Cook rice");
+    console.log ("Prepare leaves");
+    console.log ("Prepare and cut vegetables");
+    console.log ("Hand-made the sushi"); }
+
+    // Using a function
+    sushilover ();
+
+    sushilover (); sushilover (); sushilover ();
+    pizzaMediterrane (); pizzaMediterrane (); 
+
+
+
+    // Functies zijn er zodat je alle stappen niet steeds hoeft te herhalen. 
