2b66c21cc8a45c8e86cfbd53002e4eaadee03ea1
office-assistant <83004209+Eenlevenlangleren@users.noreply.github.com>
2021-10-14 19:34:59 +0200

    Add files via upload
    function arguments

---
+++ Function arguments.js
@@ -0,0 +1,55 @@
+No argument
+function wallPainter() {
+alert ('The wall has been painted red');  
+}
+wallPainter ();
+
+// Single argument
+
+function Moving (){
+  let  message = 'The wall has been printed green';
+  alert (message); }
+
+  Moving();
+
+  function Moving (){
+    let  message = 'The wall has been printed orange';
+    alert (message); }
+  
+    Moving();
+
+    // The message is not defined in below function.
+
+    function Moving (){
+        alert (message); }
+        Moving();
+
+
+    // Multiple arguments// benieuwd hoe jullie dit doen? Minst duidelijke uitleg. Verwarrend en onduidelijk. Sorry, mijn mening.
+   function walls() {
+   let =  "The north wall has been painted orange";
+   let = "The south-east wall has been painted grey";
+
+   alert (message); }
+    
+    walls();
+
+   
+
+
+
+
+  
+    
+
+
+
+
+
+
+
+
+
+
+
+
