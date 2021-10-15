560fe98395c2d72f5882c989560d0cf039aaa43d
office-assistant <83004209+Eenlevenlangleren@users.noreply.github.com>
2021-10-15 14:01:27 +0200

    Update Function arguments.js
    Aanvulling op huiswerk van eerder

--- Function arguments.js
+++ Function arguments.js
@@ -25,7 +25,12 @@ function Moving (){
         Moving();
 
 
-    // Multiple arguments// benieuwd hoe jullie dit doen? Minst duidelijke uitleg. Verwarrend en onduidelijk. Sorry, mijn mening.
+    // Multiple arguments// 
+Benieuwd hoe jullie dit doen? Dit vond ik het lastigste onderwerp. Heb het vanochtend weer een keer herhaalt en ga dat de komende dagen weer doen. 
+Meerdere argumenten gaat lastig om te schrijven. Na de tweede keer snap ik het al beter. Zit nog iets wat ik met meerdere argumenten niet begrijp. 
+Die schakeling heb ik nog niet in mijn hoofd. Ga verder met het volgende onderwerp en dan herhaal ik dit onderwerp morgen weer. De basisgedachte begrijp ik nu.
+
+
    function walls() {
    let =  "The north wall has been painted orange";
    let = "The south-east wall has been painted grey";
