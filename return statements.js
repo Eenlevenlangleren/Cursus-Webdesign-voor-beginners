2549dac60bd6123dc56edc1911835151f4a1bb2d
office-assistant <83004209+Eenlevenlangleren@users.noreply.github.com>
2021-10-18 12:29:48 +0200

    Add files via upload
    Afgelopen twee dagen het hele js stuk herhaald. Valt beter op zijn plek. Nu ga ik eerst door tot het eind om de studie af te maken en dan herhaal ik alles nog een keer.
    echt alles is nieuw. Als ik voor nu de basisprincipes maar snap. Ik ga eerst als webcaremedewerker aan de slag en dan kan ik verder stappen zetten. anders verdrink ik erin.

---
+++ return statements.js
@@ -0,0 +1,30 @@
+const compare = function (number) {
+    const number = 100;
+    if (number>100) {return "true"};
+    if (number<=100) {return "false"};
+
+}
+console.log (result)
+
+const alBouncer = function (age, people) {
+    const age = 18
+    const people = 250
+    if (age>18) {return "this is a club for adults"}
+    
+    if (people>250) {return '' It is to busy now come back later}
+    else ("come in")
+
+}
+
+console.log (result)
+
+const average = function (number 1, number 2, number 3, number 4, number 5)
+const average = number1 + number2 + number 3 + number 4 + number 5;
+Math.round = average  to a whole number
+return average;
+
+};
+
+const result = average (1,2,3,4,5);
+console.log (result);
+
