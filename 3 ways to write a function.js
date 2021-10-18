4a3e144c21ee1efe7792b2458ab3097993814779
office-assistant <83004209+Eenlevenlangleren@users.noreply.github.com>
2021-10-18 13:36:22 +0200

    Add files via upload
    Ik begrijp dat er 3 verschillende manieren van het schrijven van een functie zijn. Ik probeer overal de kern uit te pakken.

---
+++ 3 ways to write a function.js
@@ -0,0 +1,25 @@
+// Function declaration
+
+function declaration (number1, number2 )
+
+{number1*number1 + number2*number2  
+    declaration outcome and return value}
+
+declaration(9,5);
+
+// Function expression
+
+const expression = function (9,5)
+{9*9 + 5*5
+    expression outcome and return value};
+
+expression(9,5);
+
+Arrow functies
+
+const Arrow (num1, num2) pijl {
+    num1*num1 + num2*num2
+    Arrow outcome and return value
+};
+
+Arrow (9,5);
