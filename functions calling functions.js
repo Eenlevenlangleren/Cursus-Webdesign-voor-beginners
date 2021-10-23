af29f5727aa2a73acf872c5a3a426db7389eea78
office-assistant <83004209+Eenlevenlangleren@users.noreply.github.com>
2021-10-23 19:34:57 +0200

    Add files via upload

---
+++ functions calling functions.js
@@ -0,0 +1,27 @@
+const birthYear = function (age) {
+    if age >=18 return true;
+    else return false;
+}
+return birthYear;
+
+const adult = function (age) {
+    return birthYear;
+    if age >=18 return "Hello there";}
+    else return "Hey kiddo";
+};
+    console.log(adult);
+ 
+/*Ik heb nog geen enkele ervaring. Begin van scratch. Wil eerst de basisstof een keer helemaal doornemen en de 
+opgaves in js zoveel mogelijk doen. Ben aan het solliciteren. Onderhoud website hoort daarbij. Wil dan de ervaring opdoen.*/
+
+// VAT excercise 1
+const priceCalculation = function (itemPrice, VAT) {
+
+    const = sum itemprice + VAT;
+ 
+    if item = food VAT = 9%;
+    else = 0%;
+};
+
+/* Ik begrijp de studiestof maar weet niet goed hoe ik de laaste opdrachten alleen moet doen. Te weinig ervaring om het goed op te schrijven. 
+Scratch. Maar ik begrijp het wel.*/
