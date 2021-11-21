5b97787906e51e012e0487ab66f90a05882995ee
office-assistant <83004209+Eenlevenlangleren@users.noreply.github.com>
2021-11-21 11:59:40 +0100

    Add files via upload

---
+++ makecomments 2111.js
@@ -0,0 +1,13 @@
+console.log ("Hello Winc Academy students")
+
+//"This is a large sum". Comments worden niet gelezen door de javascript-engine.
+
+/*
+let multiplication = 1230941 * 1823794;
+console.log(multiplication)
+
+let subtraction = 637263 / 54;
+console.log(subtraction)*/
+
+Ik kan het resultaat van de som niet zien. Door blockcomment.
+
