8b3a958b9e766126d4eaa3f38f2b60290b844f13
office-assistant <83004209+Eenlevenlangleren@users.noreply.github.com>
2021-11-21 18:30:37 +0100

    Add files via upload

---
+++ ifelsestatements2111.js
@@ -0,0 +1,16 @@
+const age = number;
+
+if (age >=  18);
+{console.log ("You are allowed to come in");}
+else {console.log("please stay out, you are to young");}
+
+const female = boolean;
+
+if (boolean is true) { document.write ("Come in, lady");}
+else {document.write("I am sorry, gentleman. You are not allowed to come in.")}
+
+const driverStatus = 'bob';
+if(driverStatus = "bob"){ console.log("The driver has the bob status and is allowed to drive");}
+else {console.log("The driver does not have the bob status and is therefore not allowed to drive.");}
+
+
