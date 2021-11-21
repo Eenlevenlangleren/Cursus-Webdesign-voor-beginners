58f1126c9720914cf919ed963b19508bfa4c8922
office-assistant <83004209+Eenlevenlangleren@users.noreply.github.com>
2021-11-22 00:05:55 +0100

    Add files via upload

---
+++ Reference types - Objects & Arrays 2111.js
@@ -0,0 +1,53 @@
+// Part 1 - objects
+
+let name = 'Suzanne';
+let age = 55;
+
+let person = {
+name: 'Suzanne',
+age: '55',
+}
+
+// value is the array with the numbers
+let evaluations = [7,10,9];
+console.log(evaluations);
+
+
+console.log(person);
+console.log(person.name);
+console.log(person.age);
+
+person.name='adam';
+person['name']='eva';
+
+// Part 2 Arrays
+
+let array = ["green", "blue", "red"]
+console.log(array);
+
+let array = ["green", "blue", "red"]
+console.log(array.length)
+
+let array = ["green", "blue", "red"]
+console.log(array[0]);
+
+let array = ["green", "blue", "red"]
+console.log(array[array - 1 ]);
+
+let array = ["green", "blue", "red"]
+array[2] = 'yellow';
+array[3] = 5;
+array[4] = {greeting: "hi, I am an object"};
+
+console.log(array[array-1]);
+
+console.log(catBread ["American Bobtail"]);
+console,log(catBread[0], energyLevel = 5);
+console.log(catBread[1], temperament [0]);
+console.log(catBread[2], temperament [4]);
+console.log(catBread[2], write.document('favourite food: meaty things');
+
+
+
+
+
