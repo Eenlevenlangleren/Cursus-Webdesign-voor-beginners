e0ab23e92b16aa8b20f18b750819b478a48d6512
office-assistant <83004209+Eenlevenlangleren@users.noreply.github.com>
2021-11-21 19:38:51 +0100

    Add files via upload

---
+++ ifelsestatements2111operators.js
@@ -0,0 +1,42 @@
+const age = number;
+
+if (age >=  18);
+{console.log ("You are allowed to come in");}
+else {console.log("please stay out, you are to young");}
+
+const female = boolean;
+
+if (boolean is true) { document.write ("Come in, lady");}
+else {document.write("I am sorry, gentleman. You are not allowed to come in.")}
+
+const driverStatus = 'bob';
+if(driverStatus = "bob"){ console.log("The driver has the bob status and is allowed to drive");}
+else {console.log("The driver does not have the bob status and is therefore not allowed to drive.");}
+
+var age = number;
+if (age>=18 && age<= 25);
+{console.log("You get 50% discount on the price of your beer!");}
+else {console.log("Better luck next time!");}
+
+var firstName = "Suzanne";
+if(firstName = "Bram"|| firstName = "Sarah");
+{console.log("The person is entitled on a free beer on this festive day");}
+else{console.log("Next time better luck");}
+
+const totalAmountbill = number;
+const totalAmountdrinkbill = number;
+
+if (totalAmountdrinkbill > 50);
+{console.log("You get a free Veggie bitterballen");}
+
+else if {totalAmountbill > 50);
+{console.log("You get a free portion of nachos");}
+
+else if (totalAmountbill > 100);
+{console.log("You get a free bottle of champagne");}
+    
+} else {console.log("Next time better luck");}
+    
+}
+
+
