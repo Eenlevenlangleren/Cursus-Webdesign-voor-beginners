c1a26767d3038398ff07b6f1eb3f83db13ecfe5e
office-assistant <83004209+Eenlevenlangleren@users.noreply.github.com>
2021-11-21 11:36:06 +0100

    Add files via upload
    Mijn eerste javascript

---
+++ javascript2011.js
@@ -0,0 +1,26 @@
+console.log("Hello Winc Academy")
+    let name = 'Suzanne';
+    console.log(name)
+    // if is a reserved keyword, you can not use this as a label
+    let sum = 4 + 4;
+    console.log(sum)
+    let sumWithStrings = 4 + 4;
+    console.log(sumWithStrings)
+    let addition = 5 + 5;
+    console.log(addition)
+    let subtraction = 5 - 5;
+    console.log(subtraction)
+    let multiplication = 5 * 5;
+    console.log(multiplication)
+    let division = 10 / 5;
+    console.log(division)
+    let modulus = 14%6;
+    console.log(modulus)
+    let age = 55;
+    console.log(typeof age)
+    let lifeYears ='55';
+    console.log(typeof lifeYears)
+
+
+
+    
