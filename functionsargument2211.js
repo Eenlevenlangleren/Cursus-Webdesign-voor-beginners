a583046146300a437d656b07f308cc2f7fff2573
office-assistant <83004209+Eenlevenlangleren@users.noreply.github.com>
2021-11-22 20:14:21 +0100

    Add files via upload

---
+++ functionsargument2211.js
@@ -0,0 +1,42 @@
+// functions with 0 arguments
+
+const paintWall = function()
+{console.log("the wall has been painted red")};
+paintWall();
+
+// functions with 1 argument
+
+const paintWall = function(color) {console.log('The wall has been painted + color');
+};
+
+paintWall(green);
+paintWall(red);
+
+// functions with multiply arguments
+
+const paintWall = function (direction, color)
+{
+if ( direction = north && color = orange);
+console.log ('The  northwall has been painted orange' );
+if (direction = south-east && color = grey );
+console.log('The south-east wall has been painted grey');};
+
+paintWall(north, orange);
+paintWall(south-east, grey);
+
+const paintWall = function (direction, color);
+{console.log('The + 'direction' +wall has been painted + 'color'.;')};
+
+const direction = north;
+const color = orange;
+
+const direction = south-east;
+const color = grey;
+
+paintWall(south-east, grey);
+paintWall(north, orange)
+
+/ *The order of the arguments matters in some cases. 3*4 is the same as 4*3. In sentences you get a different sentence.
+The north wall has been painted orange, or the orange wall has been painted north.
+
+
