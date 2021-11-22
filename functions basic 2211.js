914c608615266136f197faaf5088028d40eb82be
office-assistant <83004209+Eenlevenlangleren@users.noreply.github.com>
2021-11-22 16:36:01 +0100

    Add files via upload

---
+++ functions basic 2211.js
@@ -0,0 +1,25 @@
+const makePizza = function()  {
+
+    console.log(make bread);
+    console.log(cute the vegetables);
+    console.log(spread the cheese);
+    console.log(put it in the oven);
+
+}
+// You have to put the function on
+
+makePizza();
+makePizza();
+makePizza();
+
+const makeSushi = function () {
+    console.log(cook rice);
+    console.log(cut vegetables);
+    console.log( roll boiled leaves);
+}
+
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
