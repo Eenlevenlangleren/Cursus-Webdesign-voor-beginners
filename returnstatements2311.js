83cfffb5b60fc502abb0cc0de6caec428fd0e230
office-assistant <83004209+Eenlevenlangleren@users.noreply.github.com>
2021-11-23 16:06:24 +0100

    Add files via upload

---
+++ returnstatements2311.js
@@ -0,0 +1,60 @@
+/* We are using the function to produce or calculate something. We use a returnstatement.
+Check if a number is Big.*/
+const numberCheck = function ()
+{if (number > 100);
+return: true;
+if (number <=100)
+return: false;
+;}
+
+numberCheck();
+
+console.log(Boolean);
+
+/* We are using the function to produce or calculate something. We use a returnstatement.
+Bouncer at a club*/
+
+const bouncerBot = function (numbers)
+{if ('The number of people in the club <= 100');
+return: 'come in':
+else('It is to busy, come back later');
+
+if('the age of the person wanting to enter the club >=18');
+return:"come in"
+else:"this is a club for adults"
+};
+
+bouncerBot(85,25);
+bouncerBot(77,55);
+
+// Anders zou ik 2 verschillende functies schrijven. Bovenstaande functie opsplitsen.
+
+/* We are using the function to produce or calculate something. We use a returnstatement.
+Calculating the average.
+
+const number1 = 10
+const number2 = 20
+const number3 = 30
+const number4 = 40
+const number5 = 50
+
+const average = function (numbers)
+{ calculateAverage of number1, number2, number3, number4, number5;
+return: Math.round };
+
+average()
+
+// -------------------------------------------------------- Herschreven
+
+const number1 = 5
+const number2 = 10
+const number3 = 15
+
+const promedio = function (numbers)
+{ calculateAverage of number1, number2, number3;
+return: average };
+
+promedio()
+
+console.log (average)
+
