1e2790ee51560d9d4a2491a3af7130cc21b2c72f
office-assistant <83004209+Eenlevenlangleren@users.noreply.github.com>
2021-11-24 13:16:19 +0100

    Add files via upload

---
+++ functions calling functions 2411.js
@@ -0,0 +1,72 @@
+// Hey kiddo
+
+const checkAge = function (number); {
+    console.log ("checkAge");
+    if (number >= 18, return true;
+    else return false;
+};
+
+const adultAge = function(number); {
+    checkAge();
+    if (age>=18  return "Hello there");
+    else return "Hey kiddo";
+    
+};
+console.log(adultAge);
+
+// VAT exercise 1
+
+
+const  priceVat = function (numbers, VAT)
+{console.log ("Entering priceVat");
+console.log (Input: number);
+console.log (Input: VAT);
+VAT 9% = 0.09 * number
+VAT21% = 0.21 * number
+const result = number + VAT;
+console.log (Output: result);
+return result;
+};
+
+const totalPrice = function (numbers) {
+    console.log ("Entering total price");
+
+    priceVat ()
+}
+    
+
+
+// VAT exercise 2
+
+const  calculatePrices = function (numbers, VAT)
+{console.log ("the amount including VAT and the VAT percentage");
+console.log (Input: amount including VAT);
+console.log (Input: VAT percentage);
+const result = total amount + VAT;
+console.log (Output: result);
+return result;
+};
+
+calculatePrices (100, 9%),
+calculatePrices (250, 21% )
+
+const totalPrices = function (numbers) {
+    console.log ("calculation total prices");
+    calculatePrices()
+};
+    
+   
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+// VAT exercise 2
