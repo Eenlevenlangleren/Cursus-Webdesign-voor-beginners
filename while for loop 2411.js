a71b4a4b488cc441be74d84b0fdc99089b1e172c
office-assistant <83004209+Eenlevenlangleren@users.noreply.github.com>
2021-11-24 21:43:44 +0100

    Add files via upload

---
+++ while for loop 2411.js
@@ -0,0 +1,48 @@
+//Even Odd Reporter
+
+while
+let i = 0; 
+while(i <=20)  {
+    determine, is ${i} even or odd?
+    console.log(number%2)
+    number ++
+};
+
+document.write ('The number is now over 20');
+    
+for (let i = 0; i <= 20; i++) {
+    determine, is ${i} even or odd?
+   console.log(number%2)
+  };
+
+  document.write ('The number is now over 20');
+
+  //Multiplication tables
+
+for(let i = 1; i <= 10; i++) {
+const result = i * number;
+console.log(`${number} * ${i} = ${result}`);
+}
+
+document.write ('You are now at number 11');
+
+//The grade assigner
+
+for (let i = 1; i >= 60 && <=100; i++) {
+    if (score > 90) {return  assignGrade "A" - For scoring 80 points, you get an C;
+    } else if (score > 80) {return assignGrade "B" - For scoring 80 points, you get an C;
+    } else if (score > 70 ) {return assignGrade "C" - For scoring 80 points, you get an C;
+    } else if (score > 65) {return assignGrade "D" - For scoring 80 points, you get an C;
+    } else  { return assignGrade "E"  - For scoring 80 points, you get an C;
+
+    return;
+    }
+
+
+  
+
+
+
+
+  
+  
