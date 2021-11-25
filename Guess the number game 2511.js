5b5ae9bf3d3f7f7745376d0442540ba4cfa8d9e4
office-assistant <83004209+Eenlevenlangleren@users.noreply.github.com>
2021-11-25 20:45:56 +0100

    Add files via upload

---
+++ Guess the number game 2511.js
@@ -0,0 +1,10 @@
+/*het maken van spelletjes is voor mij compleet nieuw. ik probeer de eerste basisstappen te zetten.
+Het interactieve zit hem in het feit dat de gebruiker invloed heeft op wat er gebeurt.
+Functionaliteiten. Wat een product kan.
+Je hebt de user en de developer naar aanleiding van de input van de user worden er stappen gezet 
+wat leidt naar het einddoel.*/
+
+/* Ik wil bezig met een goede basistutorial voor het maken van spelletjes. Ben ik naar op zoek. 
+Voor nu ben ik mij aan het verdiepen in Babylon.js. Interessant. 
+Krijg je er een beeld van hoe het in lagen opgebouwd is. Zoals ik al eerder zei. 
+Basiskennis is voor nu het belangrijkste doel*/
